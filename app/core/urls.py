5ba46efbc3319500a6e4c09866f24ac1216eed00
NikitaMaslovskiy <nikitamaslovskiy999@gmail.com>
2025-02-14 20:36:33 +0300

    fix-1_bd

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,13 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectMemberViewSet, ProjectViewSet, SubscriberViewSet, TaskViewSet
-
 router = DefaultRouter()
-router.register("projects", ProjectViewSet)
-router.register("tasks", TaskViewSet)
-router.register("project_members", ProjectMemberViewSet)
-router.register("subscribers", SubscriberViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-echo "POSTGRES_DB: $POSTGRES_DB"
-echo "POSTGRES_USER: $POSTGRES_USER"
-echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
-echo "HOST: $HOST"
-echo "PORT: $PORT"
-
 poetry run python app/manage.py makemigrations core
+poetry run python app/manage.py makemigrations tasks
+poetry run python app/manage.py makemigrations projects
 
 poetry run python app/manage.py migrate
 
