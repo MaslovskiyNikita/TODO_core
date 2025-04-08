442a69293c2803b07730815de99376187ed3bc6d
NikitaMaslovskiy <nikitamaslovskiy999@gmail.com>
2025-04-08 18:04:22 +0300

    fix

--- application/auth/requsts_to_auth/users_mail.py
+++ application/auth/requsts_to_auth/users_mail.py
@@ -4,14 +4,16 @@ from core.settings import AUTH_HOST, EMAIL_HOST_USER
 
 class UserManagementClient:
     def __init__(self):
-        self.link = AUTH_HOST + "api/users/emails"
+        self.link = AUTH_HOST
 
     def get_users_email(self, user_ids: list[str]) -> list[str]:
         """
 
+        auth_link = str(self.link) + "api/users/emails"
+
         response = requests.get(
             self.link,
-            params={"user_ids": user_ids},
+            params={"user_ids": ",".join(user_ids)},
         )
 
         users_emails = response.json()["emails"]
